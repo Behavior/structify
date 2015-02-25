b0b5fc9a5cfa0a5f00638d7a7ecbf6054df395b1
Yasuaki Uechi <uetchy@randompaper.co>
2015-02-25 22:15:10 +0900

    Add test

---
+++ spec/structify_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe Structify::Parser do
+  it 'test' do
+    sentences = [
+      "電気を消して",
+      "適当に音楽を流して",
+      "アラームを止めて窓を開けて",
+      "明日の朝の気温が低かったら夜明け前に暖房を付けておいて"
+    ]
+
+    # 原則
+    # * プラグインはストリーム形式
+    # * 類推不可能なほどコンテキストが不足する場合は追加質問をする
+
+    # 例
+    # "明日の気温が低かったら起床前に暖房を付けて"
+    # -- 明日 => 気温
+    # -- 気温 => 低い
+    # -- 起床前 => 付ける
+    # -- 暖房 => 付ける
+    # 終端ノードから解析
+    # 1. "付ける[behavior:active]" => "暖房[controller:aircon]"
+    #                             => ("前[condition:before]" => "起床[timeslot:morning]")
+    # result.
+    # activate(Aircon) if Time.now < Timeslot::Morning.begin
+    #
+    # 2. "低い[condition:low]" => "気温[fetcher:temperature]"
+    #    "気温[fetcher:temperature]" => "明日[timeslot:tomorrow]"
+
+    sentences.each do |sentence|
+      puts "\n##{sentence}"
+      parser = Structify::Parser.new
+      parser.parse(sentence)
+    end
+  end
+end
