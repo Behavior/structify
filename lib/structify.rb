d4beebe79147ff4f46ad1ec7b061b0079fb95128
Yasuaki Uechi <uetchy@randompaper.co>
2015-06-04 21:52:51 +0800

    Use plugin

--- lib/structify.rb
+++ lib/structify.rb
@@ -2,14 +2,14 @@ require 'pp'
 require 'pry-byebug'
 
 require 'structify/cabocha'
-require 'structify/controllers'
+require 'structify/plugins'
 require 'structify/parser'
 require 'structify/version'
 
 module Structify
   private
 
-  def self.controllers
+  def self.plugins
     ObjectSpace.each_object(Controllers::Base.singleton_class).select{ |klass| klass.superclass == Controllers::Base }
   end
 end
--- lib/structify/parser.rb
+++ lib/structify/parser.rb
@@ -12,6 +12,7 @@ module Structify
       @tree.chunks.each do |chunk|
         if (chunk.next_chunk && chunk.noun?)
           left, right = chunk, chunk.next_chunk
+
           particle = chunk.tokens.size > 1 ? chunk.tokens.last : 'が'
           if particle.to_s =~ /[が]/
             base, context = left, right
@@ -19,26 +20,26 @@ module Structify
             base, context = right, left
           end
 
+          # if base.tokens.first.feature_list(0) == "動詞"
+          #   puts "Do #{base.to_base} for #{context.to_base}"
+          # else
+          #   puts "Apply context '#{context.to_base}' for #{base.to_base}"
+          # end
+
           particle_tbl = {
-            "の" => "%{base}.%{context}",
-            "が" => "Exec(%{base}.receive) == %{context}",
-            "に" => "Scheduler.new(%{context}){ %{base} }", # leftが形容動詞の場合は別
-            "を" => "%{base}(%{context})"
-          }
-          aliases = {
-            "流す" => "activate",
-            "止める" => "deactivate",
-            "音楽" => Controllers::Music,
-            "明日" => "Timeslot::Tomorrow"
+            "の" => "Apply context %{context} for %{base}",
+            "が" => "%{base} == %{context}",
+            "に" => "Do %{base} when %{context}", # leftが形容動詞の場合は別
+            "を" => "Do %{base} for %{context}"
           }
 
-          puts particle_tbl[particle.to_s] % {base: ba, context: ca}
+          puts particle_tbl[particle.to_s] % {base: base.to_base, context: context.to_base}
 
-          @nodes[ba] ||= []
-          if @nodes[ba].include? ca.to_s
-            @nodes[ba] << ca.new(@nodes[ba].to_s)
+          @nodes[base.to_base] ||= []
+          if @nodes[base.to_base].include? context.to_base.to_s
+            @nodes[base.to_base] << context.to_base.to_s(@nodes[base.to_base].to_s)
           else
-            @nodes[ba] << ca.new
+            @nodes[base.to_base] << context.to_base.to_s
           end
         end
       end
